b9b35d5ce7cb61b4ce0251db13d23a41a0d2a39a
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-10 22:47:14 +0530

    Add student enrollment input and output display

--- Basic_Python/Day_01_io.py
+++ Basic_Python/Day_01_io.py
@@ -1,1 +1,17 @@
+print("--- MSBTE Student Enrollment System ---")
 
+# 1. Taking Inputs From the User
+name = input("Enter your Full Name: ")
+enrollment_no = input("Enter your Enrollment Number: ")
+branch = input("Enter your Branch (Example: CO/IF): ")
+sem_percentage = float(input("Enter your last Semester Percentage: "))
+
+# 2. Displaying Outputs (Data Presentation)
+print("="*30) #"="*30 it shows the =============== the lines for good-looking
+print("STUDENT PROFILE CREATED")
+print("="*30)
+print(f"Name         : {name}")
+print(f"Enrollment   : {enrollment_no}")
+print(f"Department   : {branch}")
+print(f"Performance  : {sem_percentage}%")
+print("="*30) #"="*30 it shows the =============== the lines for good-looking
