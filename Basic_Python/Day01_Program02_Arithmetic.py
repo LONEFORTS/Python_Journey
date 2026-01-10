8770072f0bab1b59ce7e492d4ca1f0616cff4959
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-10 22:53:42 +0530

    Add files via upload

---
+++ Basic_Python/Day01_Program02_Arithmetic.py
@@ -0,0 +1,14 @@
+# MSBTE Practical No. 3 - Arithmetic Logic
+# Purpose: Demonstrating Math Operators
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(f"Remainder (Modulus): {num1 % num2}")
+print(f"Floor Division: {num1 // num2}")
+print(f"Power: {num1 ** num2}")
+    
