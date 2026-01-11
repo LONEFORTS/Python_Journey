62ff94598a80b531305aab577622630a2b083dbc
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-11 17:20:58 +0530

    Create Day02_Program13_positive_negative_zero.py
    MSBTE_PP_PN04QN03(iii)

---
+++ Control_Flow_Python/Day02_Program13_positive_negative_zero.py
@@ -0,0 +1,10 @@
+#Program to check positive, negative or zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+print("Positive Number")
+elif num < 0:
+print("Negative Number")
+else:
+print("Zero")
