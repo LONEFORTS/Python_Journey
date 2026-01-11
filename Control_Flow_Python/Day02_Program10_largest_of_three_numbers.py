2f078bb5f621f72634a5854993c07a9e5e20f5f1
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-11 17:14:31 +0530

    Create Day02_Program10_largest_of_three_numbers.py
    MSBTE_PP_PN04QN02

---
+++ Control_Flow_Python/Day02_Program10_largest_of_three_numbers.py
@@ -0,0 +1,12 @@
+#Follwing Program is it show the largest Number among three Numbers 
+
+Number1= int(input("Enter first number: "))
+Number1= int(input("Enter second number: "))
+Number3= int(input("Enter third number: "))
+
+if a >= b and a >= c:
+print("Largest number is:", a)
+elif b >= a and b >= c:
+print("Largest number is:", b)
+else:
+print("Largest number is:", c)
