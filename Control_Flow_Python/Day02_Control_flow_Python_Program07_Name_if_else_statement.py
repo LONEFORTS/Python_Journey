1052a91710d04cf0b7f7dc503043492c5bc85f2f
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-11 16:36:49 +0530

    Create Day02_Control_flow_Python_Program07_Name_if_else_statement.py
    MSBTE_PP_PN04

---
+++ Control_Flow_Python/Day02_Control_flow_Python_Program07_Name_if_else_statement.py
@@ -0,0 +1,16 @@
+
+# This program uses the if-else control flow statement to check
+whether a given integer number is EVEN or ODD.
+
+
+# Take input from the user
+num = int(input("Enter an integer number: "))
+
+# Check even or odd using if-else
+if num % 2 == 0:
+    print("The given number is EVEN.")
+else:
+    print("The given number is ODD.")
+
+# Program end message
+print("Program execution completed.")
