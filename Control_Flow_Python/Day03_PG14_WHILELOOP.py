ea66d78f87cd2b3fcaa318064c83ce7b0226e576
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-12 10:53:03 +0530

    Add countdown timer using while loop
    Implement countdown timer using a while loop.

---
+++ Control_Flow_Python/Day03_PG14_WHILELOOP.py
@@ -0,0 +1,10 @@
+#Following program is of while loop its  Goal is to  Repeat logic until a specific condition is met
+
+print("--- Countdown Timer ---")
+count = 5
+
+while count > 0:
+    print(f"System starting in... {count}")
+    count -= 1  # Decrementing the counter
+
+print("System Active!")
