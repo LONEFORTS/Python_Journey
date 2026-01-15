8a3aa2bbce7c96f9ba0960d6778a4ce92a70baf2
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-15 17:19:35 +0530

    Add files via upload
    THIS PROGRAM DEMONSTRATES SIMPLE BUILT IN FUNCTIONS IN PYTHON WHILE THE PROGRAM IS BASIC BUT TELL BASIC FUNCTIONS IN PYTHON'S LIST WHICH ARE BUILT-IN.

---
+++ BUILTin_DATA_STRUCTURES_PYTHON/Day06_Program33_BUILTinFUNCTIONSin_LIST.py
@@ -0,0 +1,33 @@
+#There are so many functions in the list.
+#One thing can be Mentioned as Many people think that Functions and methods are same While it's not true. 
+#follwing are definitions of both function and method in context of LIST in python are: 
+#FUNCTION : i) function is a block of code in python. ii) Function take LIST as an argument and return a value. 
+#Always think whenever there will be the syntax or you see the list name in the () it will be the function. 
+#THere are so many Built In functions in the list some of those are following shown in the program;
+
+List1 = [10, 20, 30, 40, 50]
+List2 = [40, 50, 60, 70, 80,90,100]
+
+# 1) len() function returns the length of the list and \n represents new line here: 
+print(f"\nLength of List1 is:{len(List1)}\nLength of List2 is:{len(List2)} ")
+
+# 2) max() function returns the MAXIMUM value of the list
+print(f"\nMAX value in the List1 is: {max(List1)}\nMAX value in the List2 is: {max(List2)}")
+
+# 3) min() function returns the minimum value from the given list 
+print(f"\nMINIMUM value in the List1 is : {min(List1)}\nMINIMUM value in the List2 is: {min(List2)}")  
+
+# 4) Theres is also a sum()_used in the list for making the addition of the elements of list as well as we can also combined and get addition of two list here we will see both addition of elements in one list as well as addition elements combining both lists : 
+# here is the simple one addition of elements of list1 
+print(f"\nSum of list 1 is {sum(List1)}")
+#here is the addition of elements of both lists here's a simple trick that we can make addition of both list using + operator
+
+print(f"Sum of List1 and List 2 is : {sum(List1) + sum(List2)}")
+
+#following are some more functions used in the list are: 1)cmp() - used to compare list in python 2 (Currently we are in python 3 ) 2) list() - it is used to convert the tuple into the list.
+
+
+
+#THERE ARE MORE BUILT IN FUNCTIONS OF LIST CAN BE SEEN IN PART2 OR IN PR+OGRAM no - 33
+
+    
