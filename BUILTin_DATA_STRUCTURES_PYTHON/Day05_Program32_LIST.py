2b8fbc3a50c0ba05152f24019de5e8e5bd960509
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-15 16:40:07 +0530

    Update Day05_Program32_LIST.py

--- BUILTin_DATA_STRUCTURES_PYTHON/Day05_Program32_LIST.py
+++ BUILTin_DATA_STRUCTURES_PYTHON/Day05_Program32_LIST.py
@@ -1,1 +1,9 @@
+#List is one of Built-In Data Strucutre of Python
+#Simply a list can be defined as : collection of values or items of different types.
 
+#follwing is a simple LIST program: 
+#syntax of list : List_Name = [items,'items',item] remember '' single quote is used for strings 
+
+
+marks=[10,20,30,40,50] 
+print(marks) 
