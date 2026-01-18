8a8dbecfcb148deba2ff9f8fc4ef75a6daf72f56
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-18 23:43:42 +0530

    Create Day08_Program51_DictionaryDelete.py

---
+++ BUILTin_DATA_STRUCTURES_PYTHON/DICTIONARY_PYTHON/Day08_Program51_DictionaryDelete.py
@@ -0,0 +1,6 @@
+student = {"name": "Arjun", "age": 17, "grade": "A", "city": "Mumbai"}
+# Remove 'city' using pop
+student.pop("city")
+# Remove 'grade' using del
+del student["grade"]
+print(student)
